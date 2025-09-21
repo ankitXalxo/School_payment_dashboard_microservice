92dc6cf4f73f977f150fac39330e68f1b63d496f
Ankit_Kishore_Khalkho <ankitjasonxalxo9@gmail.com>
2025-09-21 20:45:05 +0530

    Fixed logout endpoint

--- frontend-school/src/utils/logout.js
+++ frontend-school/src/utils/logout.js
@@ -1,4 +1,4 @@
 export const logout = () => {
   localStorage.removeItem("token"); // remove auth token
-  window.location.href = "/login"; // redirect to login page
+  window.location.href = "auth/login"; // redirect to login page
 };
